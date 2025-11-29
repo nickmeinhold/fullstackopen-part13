bfe2d8a21b5d25812b9dfb33eb1847d33402731b
Nick Meinhold <nick@enspyr.co>
2025-11-29 11:24:22 +1100

    Complete Exercise 13.13

--- blogs/controllers/blogs.js
+++ blogs/controllers/blogs.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken");
 
 const { Blog, User } = require("../models");
 const { SECRET } = require("../utils/config");
+const { where } = require("sequelize");
 
 const tokenExtractor = (req, res, next) => {
   const authorization = req.get("authorization");
@@ -23,7 +24,16 @@ const tokenExtractor = (req, res, next) => {
 };
 
 router.get("/", async (req, res) => {
+  const where = {};
+
+  if (req.query.search) {
+    where.title = {
+      [Op.substring]: req.query.search,
+    };
+  }
+
   const blogs = await Blog.findAll({
+    where,
     attributes: { exclude: ["userId"] },
     include: {
       model: User,
